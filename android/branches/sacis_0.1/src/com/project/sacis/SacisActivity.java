ecfd6f5ce4927d56d47a12b9af6908742de88a00
davi.correa <davi.correa@uniriotec.br>
2012-01-26 01:05:25 +0000

    Initial import.

---
+++ android/branches/sacis_0.1/src/com/project/sacis/SacisActivity.java
@@ -0,0 +1,13 @@
+package com.project.sacis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SacisActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
