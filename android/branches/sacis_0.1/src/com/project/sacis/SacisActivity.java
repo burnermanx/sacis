0def9c547b265318a50a3853213aaecbcd2e582a
davi.correa <davi.correa@uniriotec.br>
2012-01-26 02:33:17 +0000

    - Criada tela para criação do usuário pelo administrador.

--- android/branches/sacis_0.1/src/com/project/sacis/SacisActivity.java
+++ android/branches/sacis_0.1/src/com/project/sacis/SacisActivity.java
@@ -1,13 +1,25 @@
 package com.project.sacis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SacisActivity extends Activity {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /**
+     * Ação ao clicar no botão de sistema administrador.
+     * */
+    public void showAdminView(View view)
+    {
+    	Intent sysAdminIntent = new Intent(view.getContext(), SysAdminActivity.class);
+    	startActivityForResult(sysAdminIntent, 0);
     }
 }
