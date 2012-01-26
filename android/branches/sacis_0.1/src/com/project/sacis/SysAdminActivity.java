b51ac29e2240c7746043997b4c6b70bcb97789b6
davi.correa@uniriotec.br <davi.correa@uniriotec.br@51eff9cb-53d8-8e64-2bb5-d6a5f3f1de4d>
2012-01-26 02:33:17 +0000

    - Criada tela para criação do usuário pelo administrador.

---
+++ android/branches/sacis_0.1/src/com/project/sacis/SysAdminActivity.java
@@ -0,0 +1,21 @@
+package com.project.sacis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Proposito da classe:
+ * TODO: explicar proposito da classe
+ *
+ * @author Davi - since 25/01/2012
+ */
+public class SysAdminActivity extends Activity {
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sys_admin);
+        
+    }	
+}
