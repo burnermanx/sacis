dbfdeece58acbb7f3b84751dabbc5922baa70d10
femtokiller@gmail.com <femtokiller@gmail.com@51eff9cb-53d8-8e64-2bb5-d6a5f3f1de4d>
2012-03-01 00:19:34 +0000

    Feita a correçao no codigo da remoção de contatos pessoais e nos comentários

--- windows/branches/sacis.model.entidades/preMensagem.cs
+++ windows/branches/sacis.model.entidades/preMensagem.cs
@@ -1,8 +1,8 @@
-﻿/*
- * Classe para objetos do tipo preMensagem 
- *
- * @author Fabio Augusto
- */
+﻿///<summary>
+/// Classe para objetos do tipo preMensagem 
+///
+/// @author Fabio Augusto
+///</summary>
 
 using System;
 using System.Collections.Generic;
@@ -22,6 +22,30 @@ namespace sacis.model.entidades
         private HashSet<string> _arquivoCripto;
         private HashSet<string> _arquivoPlain;
 
+        ///<summary>
+        ///
+        /// Método contrutor para inicializar o objeto
+        ///
+        ///</summary>
+        public preMensagem() { }
+
+        ///<summary>
+        ///
+        /// Método contrutor para inicializar o objeto atraves de parâmetros
+        ///
+        ///</summary>
+        public preMensagem(string from, string to, string subject, string text, bool cript, bool sign, HashSet<string> cripto, HashSet<string> plain)
+        {
+            _de = from;
+            _para = to;
+            _assunto = subject;
+            _texto = text;
+            _criptografar = cript;
+            _assinar = sign;
+            _arquivoCripto = cripto;
+            _arquivoPlain = plain;
+        }
+        
         ///<summary>
         ///
         /// Métodos de, para, texto, criptografar, assinar, arquivoCripto e arquivoPlain
@@ -78,252 +102,162 @@ namespace sacis.model.entidades
 
         ///<summary>
         ///
-        /// Método contrutor para inicializar o objeto
+        /// Método get para o remetente
         ///
         ///</summary>
-        public preMensagem() { }
-
-        /**
-        *
-        * Método contrutor para inicializar o objeto
-        *
-        */
-        public preMensagem(string from, string to, string subject, string text, bool cript, bool sign, HashSet<string> cripto, HashSet<string> plain)
-        {
-
-            _de = from;
-            _para = to;
-            _assunto = subject;
-            _texto = text;
-            _criptografar = cript;
-            _assinar = sign;
-            _arquivoCripto = cripto;
-            _arquivoPlain = plain;
-
-        }
-
-        /**
-        *
-        * Método get para a variavel _de
-        *
-        * @return _de       Variavel do tipo String      
-        *
-        */
         public string getDe()
         {
-
             return _de;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _de
-        *
-        * @param from       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para o remetente
+        ///
+        ///</summary>
         public void setDe(string from)
         {
-
             _de = from;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _para
-        *
-        * @return _para       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para o destinatario
+        ///
+        ///</summary>
         public string getPara()
         {
-
             return _para;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _para
-        *
-        * @param to       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para o destinatario
+        ///
+        ///</summary>
         public void setPara(string to)
         {
-
             _para = to;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _assunto
-        *
-        * @return _assunto       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para o assunto
+        ///
+        ///</summary>
         public string getAssunto()
         {
-
             return _assunto;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _assunto
-        *
-        * @param subject       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para o assunto
+        ///
+        ///</summary>
         public void setAssunto(string subject)
         {
-
             _assunto = subject;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _texto
-        *
-        * @return _texto       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para o texto da mensagem
+        ///
+        ///</summary>
         public string getTexto()
         {
-
             return _texto;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _texto
-        *
-        * @param text       Variavel do tipo String      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para o texto da mensagem
+        ///
+        ///</summary>
         public void setTexto(string text)
         {
-
             _texto = text;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _criptografar
-        *
-        * @return _criptografar       Variavel do tipo bool      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para saber se a mensagem é criptografada
+        ///
+        ///</summary>
         public bool getCriptografar()
         {
-
             return _criptografar;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _criptografar
-        *
-        * @param cript       Variavel do tipo bool      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para saber se a mensagem é criptografada
+        ///
+        ///</summary> 
         public void setCriptografar(bool cript)
         {
-
             _criptografar = cript;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _assinar
-        *
-        * @return _assinar       Variavel do tipo bool      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para saber se a mensagem é assinada
+        ///
+        ///</summary>
         public bool getAssinar()
         {
-
             return _assinar;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _assinar
-        *
-        * @param sign       Variavel do tipo bool      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para saber se a mensagem é assinada
+        ///
+        ///</summary>
         public void setAssinar(bool sign)
         {
-
             _assinar = sign;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _arquivoCripto
-        *
-        * @return _arquivoCripto       Variavel do tipo HashSet<string>      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para os arquivos a serem criptografados
+        ///
+        ///</summary>
         public HashSet<string> getArquivoCripto()
         {
-
             return _arquivoCripto;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _arquivoCripto
-        *
-        * @param _arquivoCripto       Variavel do tipo HashSet<string>      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para os arquivos a serem criptografados
+        ///
+        ///</summary>
         public void setArquivoCripto(HashSet<string> cripto)
         {
-
             _arquivoCripto = cripto;
-
         }
 
-        /**
-        *
-        * Método get para a variavel _arquivoPlain
-        *
-        * @return _arquivoPlain       Variavel do tipo HashSet<string>      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método get para os arquivos claros
+        ///
+        ///</summary>
         public HashSet<string> getArquivoPlain()
         {
-
             return _arquivoPlain;
-
         }
 
-        /**
-        *
-        * Método set para a variavel _arquivoPlain
-        *
-        * @param _arquivoPlain       Variavel do tipo HashSet<string>      
-        *
-        */
+        ///<summary>
+        ///
+        /// Método set para os arquivos claros
+        ///
+        ///</summary>
         public void setArquivoPlain(HashSet<string> plain)
         {
-
             _arquivoPlain = plain;
-
         }
 
 
