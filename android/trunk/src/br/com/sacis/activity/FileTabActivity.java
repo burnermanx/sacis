f88dffb3b92eb37bd75ad9d927f313627730e1e3
davi.correa@uniriotec.br <davi.correa@uniriotec.br@51eff9cb-53d8-8e64-2bb5-d6a5f3f1de4d>
2012-07-27 04:39:17 +0000

    - Criada a activity com tab para o sistema de arquivos e adicionada no manifest.

---
+++ android/trunk/src/br/com/sacis/activity/FileTabActivity.java
@@ -0,0 +1,21 @@
+package br.com.sacis.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import greendroid.app.GDTabActivity;
+
+public class FileTabActivity extends GDTabActivity
+{
+	private static final String TAB1 = "crypt";
+	private static final String TAB2 = "decrypt";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setTitle("Sistema de Arquivos");
+		Intent intent = new Intent(getApplicationContext(), FileChooserActivity.class);
+		addTab(TAB1, "Criptografar", intent);
+		addTab(TAB2, "Descriptografar", intent);
+	}
+}
