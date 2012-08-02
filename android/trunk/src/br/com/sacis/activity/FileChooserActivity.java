5af79281da11e949d9ecc3f77a352c260513ae40
davi.correa@uniriotec.br <davi.correa@uniriotec.br@51eff9cb-53d8-8e64-2bb5-d6a5f3f1de4d>
2012-08-02 04:51:44 +0000

    - Criada a classe para trabalhar com as cifrar simetricas; - Mudanca no xml de layout do user_system para ficar aceitavel em landscape; - Adicionado botao de salvar no file_chooser; - Criada a acao para escolher o arquivo e o mesmo eh adicionado na lista.

--- android/trunk/src/br/com/sacis/activity/FileChooserActivity.java
+++ android/trunk/src/br/com/sacis/activity/FileChooserActivity.java
@@ -1,7 +1,12 @@
 package br.com.sacis.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.sacis.R;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,14 +14,20 @@ import android.widget.ListView;
 
 public class FileChooserActivity extends Activity
 {
+	private final int AttachFileRequestCode = 0;
+	
+	private ArrayAdapter<String> arrayAdapter;
+	
+	private List<String> listViewItems = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.file_chooser);
 		ListView listView = getListView();
-		String[] test = { "file1", "file2" };
-		listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, test));
+		arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, listViewItems);
+		listView.setAdapter(arrayAdapter);
 		listView.setItemsCanFocus(false);
 		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
@@ -31,6 +42,52 @@ public class FileChooserActivity extends Activity
 	 * @param view
 	 */
 	public void chooseFile(View view)
+	{
+		//Por agora ira escolher um arquivo, sera implementada uma fileDialog para contornar esse problema
+		Intent fileChooserIntent = new Intent();
+		fileChooserIntent.setAction(Intent.ACTION_GET_CONTENT);
+		fileChooserIntent.setType("text/plain");
+		startActivityForResult(
+				Intent.createChooser(fileChooserIntent, "Selecione um arquivo"),
+				AttachFileRequestCode);
+	}
+	
+	/**
+	 * Ao receber o resultado de uma atividade
+	 * */
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent)
+	{
+		if (isFileChooseOk(requestCode, resultCode))
+		{
+			Uri uri = intent.getData();
+			if (uri != null)
+			{
+				String path = uri.getPath();
+				listViewItems.add(path);
+				arrayAdapter.notifyDataSetChanged();
+			}
+		}
+	}
+
+	/**
+	 * Verifica se a requisição para escolher o arquivo foi concluida com
+	 * sucesso.
+	 * 
+	 * @param requestCode
+	 * @param resultCode
+	 * @return {@link Boolean}
+	 * */
+	private boolean isFileChooseOk(final int requestCode, final int resultCode)
+	{
+		return (requestCode == AttachFileRequestCode && resultCode == RESULT_OK);
+	}
+	
+	/**
+	 * Ação ao clicar no botao de Salvar.
+	 * @param view
+	 */
+	public void saveFiles(View view)
 	{
 		
 	}
